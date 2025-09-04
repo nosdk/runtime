368e9b0f118af2b1818fec0207e9c0251dfcf0f2
Tim Lebel <tim.lebel@gmail.com>
2025-09-03 23:27:18 -0400

    Added producers

--- kafka.c
+++ kafka.c
@@ -1,7 +1,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <librdkafka/rdkafka.h>
-#include <poll.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
@@ -10,14 +9,31 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-struct nosdk_kafka_consumer {
+enum nosdk_kafka_type {
+    PRODUCER,
+    CONSUMER,
+};
+
+struct nosdk_kafka {
+    enum nosdk_kafka_type type;
     rd_kafka_t *rk;
     pthread_t thread;
     char *topic;
 };
 
+int nosdk_ensure_fifo(char *path) {
+    struct stat st;
+    if (stat(path, &st) == -1 && errno == ENOENT) {
+        if (mkfifo(path, S_IRWXU) < 0) {
+            perror("fifo creation");
+            return -1;
+        }
+    }
+    return 0;
+}
+
 void *nosdk_kafka_consumer_thread(void *arg) {
-    struct nosdk_kafka_consumer *consumer = (struct nosdk_kafka_consumer *)arg;
+    struct nosdk_kafka *consumer = (struct nosdk_kafka *)arg;
     printf("creating consumer fifo: %s\n", consumer->topic);
 
     // ignore SIGPIPE so we can handle it
@@ -58,14 +74,47 @@ void *nosdk_kafka_consumer_thread(void *arg) {
     return NULL;
 }
 
-void nosdk_kafka_consumer_start(struct nosdk_kafka_consumer *consumer) {
-    pthread_create(
-        &consumer->thread, NULL, nosdk_kafka_consumer_thread, consumer);
+void *nosdk_kafka_producer_thread(void *arg) {
+    struct nosdk_kafka *producer = (struct nosdk_kafka *)arg;
+    printf("creating producer fifo: %s\n", producer->topic);
+
+    if (nosdk_ensure_fifo(producer->topic) < 0) {
+        return NULL;
+    }
+
+    // kafka max message size is 1mb!
+    char *msg_buf = malloc(1000 * 1000);
+
+    while (1) {
+        int read_fd = open(producer->topic, O_RDONLY);
+        if (read_fd < 0) {
+            perror("opening fifo");
+            break;
+        }
+
+        ssize_t result = read(read_fd, msg_buf, 1000 * 1000);
+
+        rd_kafka_producev(
+            producer->rk, RD_KAFKA_V_TOPIC(producer->topic),
+            RD_KAFKA_V_VALUE(msg_buf, result), RD_KAFKA_V_END);
+
+        close(read_fd);
+    }
+
+    rd_kafka_flush(producer->rk, 5000);
+    free(msg_buf);
+    return NULL;
 }
 
-void nosdk_kafka_consumer_wait(struct nosdk_kafka_consumer *consumer) {
-    pthread_join(consumer->thread, NULL);
+void nosdk_kafka_start(struct nosdk_kafka *k) {
+    if (k->type == CONSUMER) {
+        pthread_create(&k->thread, NULL, nosdk_kafka_consumer_thread, k);
+    } else if (k->type == PRODUCER) {
+        pthread_create(&k->thread, NULL, nosdk_kafka_producer_thread, k);
+    }
 }
+
+void nosdk_kafka_wait(struct nosdk_kafka *k) { pthread_join(k->thread, NULL); }
 
 void kafka_conf_must_set(
     rd_kafka_conf_t *conf, const char *property, const char *envvar) {
@@ -82,7 +131,7 @@ void kafka_conf_must_set(
     }
 }
 
-int nosdk_kafka_consumer_init(struct nosdk_kafka_consumer *consumer) {
+int nosdk_kafka_consumer_init(struct nosdk_kafka *consumer) {
     rd_kafka_conf_t *conf;
     rd_kafka_topic_partition_list_t *subscription;
     char errstr[512];
@@ -119,4 +168,32 @@ int nosdk_kafka_consumer_init(struct nosdk_kafka_consumer *consumer) {
     }
 
     return 0;
+}
+
+int nosdk_kafka_producer_init(struct nosdk_kafka *producer) {
+    rd_kafka_conf_t *conf;
+    char errstr[512];
+
+    conf = rd_kafka_conf_new();
+
+    kafka_conf_must_set(
+        conf, "bootstrap.servers", "NOSDK_KAFKA_BOOTSTRAP_SERVERS");
+
+    producer->rk =
+        rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
+    if (!producer->rk) {
+        fprintf(stderr, "failed to create producer: %s\n", errstr);
+        return 1;
+    }
+
+    return 0;
+}
+
+int nosdk_kafka_init(struct nosdk_kafka *k) {
+    if (k->type == CONSUMER) {
+        return nosdk_kafka_consumer_init(k);
+    } else if (k->type == PRODUCER) {
+        return nosdk_kafka_producer_init(k);
+    }
+    return -1;
 }
--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include "kafka.c"
 
 int main(int argc, char *argv[]) {
-    struct nosdk_kafka_consumer consumer = {
+    struct nosdk_kafka consumer = {
+        .type = CONSUMER,
         .topic = "records",
     };
 
-    int ret = nosdk_kafka_consumer_init(&consumer);
+    struct nosdk_kafka producer = {
+        .type = PRODUCER,
+        .topic = "stuff",
+    };
+
+    int ret = nosdk_kafka_init(&consumer);
     if (ret != 0) {
         return ret;
     }
 
-    nosdk_kafka_consumer_start(&consumer);
+    ret = nosdk_kafka_init(&producer);
+    if (ret != 0) {
+        return ret;
+    }
 
-    nosdk_kafka_consumer_wait(&consumer);
+    nosdk_kafka_start(&consumer);
+    nosdk_kafka_start(&producer);
+
+    nosdk_kafka_wait(&consumer);
+    nosdk_kafka_wait(&producer);
 
     return 0;
 }
