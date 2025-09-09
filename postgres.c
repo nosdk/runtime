464162d663de799ef5e792200a06496022d542eb
Tim Lebel <tim.lebel@datavant.com>
2025-09-09 13:38:11 -0400

    postgres insert

--- postgres.c
+++ postgres.c
@@ -1,9 +1,13 @@
 #include <libpq-fe.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "http.h"
 #include "postgres.h"
+#include "util.h"
 
 void nosdk_pg_disconnect(PGconn *conn) { PQfinish(conn); }
 
@@ -19,6 +23,85 @@ PGconn *nosdk_pg_connect() {
     return conn;
 }
 
+int table_exists(PGconn *conn, const char *table_name) {
+    const char *query = "SELECT EXISTS ("
+                        "SELECT 1 FROM information_schema.tables "
+                        "WHERE table_schema = 'public' AND table_name = $1"
+                        ")";
+
+    const char *params[1] = {table_name};
+    PGresult *res = PQexecParams(conn, query, 1, NULL, params, NULL, NULL, 0);
+
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+        fprintf(stderr, "Query failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        return -1;
+    }
+
+    int exists = 0;
+    if (PQntuples(res) > 0) {
+        char *result = PQgetvalue(res, 0, 0);
+        exists = (result[0] == 't');
+    }
+
+    PQclear(res);
+    return exists;
+}
+
+int create_table_jsonb(PGconn *conn, const char *table_name) {
+    char query[256];
+    snprintf(
+        query, sizeof(query),
+        ("CREATE TABLE %s ("
+         "id SERIAL PRIMARY KEY,"
+         "data JSONB NOT NULL)"),
+        table_name);
+
+    PGresult *res = PQexec(conn, query);
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        fprintf(stderr, "insert failed: %s\n", PQerrorMessage(conn));
+        PQclear(res);
+        return -1;
+    }
+
+    PQclear(res);
+    return 0;
+}
+
+void nosdk_pg_handle_post(struct nosdk_http_request *req, PGconn *conn) {
+    char *data = nosdk_http_request_body_alloc(req);
+
+    char *table_prefix = "/db/tables";
+
+    char *table_name = &req->path[strlen(table_prefix) + 1];
+    if (!table_exists(conn, table_name)) {
+        if (create_table_jsonb(conn, table_name) != 0) {
+            char *response = "HTTP/1.1 500 Internal Error";
+            write(req->client_fd, response, strlen(response));
+            return;
+        }
+    }
+
+    struct nosdk_string_buffer *sb = nosdk_string_buffer_new();
+
+    nosdk_string_buffer_append(
+        sb, "INSERT INTO %s (data) VALUES ('%s'::jsonb)", table_name, data);
+
+    sb->data[sb->size] = '\0';
+
+    printf("statement: %.*s\n", sb->size, sb->data);
+
+    PGresult *res = PQexec(conn, sb->data);
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        fprintf(stderr, "insert failed: %s\n", PQerrorMessage(conn));
+    }
+
+    PQclear(res);
+
+    char *response = "HTTP/1.1 200 OK";
+    write(req->client_fd, response, strlen(response));
+}
+
 void nosdk_pg_handler(struct nosdk_http_request *req) {
     printf("postgres handler %d %s\n", req->method, req->path);
 
@@ -29,11 +112,12 @@ void nosdk_pg_handler(struct nosdk_http_request *req) {
         return;
     }
 
-    char *data = nosdk_http_request_body_alloc(req);
-    printf("body data: %s\n", data);
-
-    char *response = "HTTP/1.1 200 OK";
-    write(req->client_fd, response, strlen(response));
+    if (req->method == HTTP_METHOD_POST) {
+        nosdk_pg_handle_post(req, conn);
+    } else {
+        char *response = "HTTP/1.1 404 Not Found";
+        write(req->client_fd, response, strlen(response));
+    }
 
     nosdk_pg_disconnect(conn);
 }
--- util.h
+++ util.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 extern int nosdk_debug_flag;
 
@@ -16,6 +17,48 @@ static inline int nosdk_debugf(const char *format, ...) {
     int result = vprintf(format, args);
     va_end(args);
     return result;
+}
+
+struct nosdk_string_buffer {
+    char *data;
+    int capacity;
+    int size;
+};
+
+static inline struct nosdk_string_buffer *nosdk_string_buffer_new() {
+    struct nosdk_string_buffer *sb = (struct nosdk_string_buffer *)malloc(
+        sizeof(struct nosdk_string_buffer));
+    sb->capacity = 1024;
+    sb->size = 0;
+    sb->data = (char *)malloc(1024);
+    return sb;
+}
+
+static inline int nosdk_string_buffer_append(
+    struct nosdk_string_buffer *sb, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    int needed = vsnprintf(NULL, 0, format, args);
+    va_end(args);
+
+    if (sb->size + needed >= sb->capacity) {
+        sb->capacity = (sb->size + needed + 1) * 2;
+        sb->data = (char *)realloc(sb->data, sb->capacity);
+    }
+
+    va_start(args, format);
+    vsnprintf(sb->data + sb->size, sb->capacity - sb->size, format, args);
+    va_end(args);
+
+    sb->size += needed;
+
+    return 0;
+}
+
+static inline void nosdk_string_buffer_free(struct nosdk_string_buffer *sb) {
+    free(sb->data);
+    free(sb);
 }
 
 #endif // _NOSDK_UTIL_H
